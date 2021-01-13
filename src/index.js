702dfd6258eb916e07b63fc899a1359f82607ed0
pzjiang <pzjiang01@hotmail.com>
2021-01-12 19:49:22 -0500

    added features to the game to allow tracking squares clicked

--- src/index.js
+++ src/index.js
@@ -3,20 +3,49 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 
+
 class Square extends React.Component {
+    /*
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+    */
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button
+                className="square"
+                onClick={() => { this.props.onClick() }}>
+
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square />;
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
     }
+
+    handleclick(i) {
+        const squares = this.state.squares.slice();
+        square[i] = 'X';
+        this.setState({ squares: squares });
+    }
+
+    renderSquare(i) {
+        return (<Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)} />
+        );
+    }
+
 
     render() {
         const status = 'Next player: X';
